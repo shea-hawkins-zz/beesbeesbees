4106de6e3eb8d20ba7d142458f62facd6374afa0
Shea Hawkins <Shea Hawkins>
2016-06-13 11:49:16 -0700

    Implement ForagerBee.

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,12 @@
 var RetiredForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canfly = true;
+  this.treasureChest = [];
 };
-
+RetiredForagerBee.prototype = Object.create(Bee.prototype);
+RetiredForagerBee.prototype.constructor = ForagerBee;
+RetiredForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
